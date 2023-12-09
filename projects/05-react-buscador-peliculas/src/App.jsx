1be11920b3e69fdf9b0e31a75c87809e81c4cec4
Yohan D Osorio L <yohan.osorio@arroyoconsulting.net>
2023-12-09 18:09:02 -0500

    Add fifth project

---
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,62 @@
+import './App.css'
+import { useMovies } from './hoocks/useMovies.js'
+import { useSearch } from './hoocks/useSearch.js'
+import { Movies } from './components/Movies.jsx'
+import { useCallback, useState } from 'react'
+import debounce from 'just-debounce-it'
+
+function App() {
+  const [sort, setSort] = useState(false)
+  const { search, updateSearch, error } = useSearch()
+  const { movies, loading, getMovies } = useMovies({ search, sort })
+
+  const debounceGetMovies = useCallback(
+    debounce(search => {
+      getMovies({ search })
+    }, 1000)
+    , [getMovies]
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ search })
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debounceGetMovies(newSearch)
+  }
+ 
+  return (
+    <div className='page'>
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }}
+            onChange={handleChange} value={search} name='query' type="text" placeholder='Avengers, The Matrix...'
+          />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <button type='submit'>Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ projects/05-react-buscador-peliculas/src/services/movies.js
@@ -0,0 +1,20 @@
+const API_KEY = 'bb22b4a8'
+
+export const searchMovies = async ({ search }) => {
+  if (search === '') return null
+
+  try {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+    const json = await response.json()
+    const movies = json.Search
+
+    return movies?.map(movie => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      image: movie.Poster
+    }))
+  } catch (error) {
+    throw new error('Error searching movies')
+  }
+}
